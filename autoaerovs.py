c30a339fa4a08cf24e725c9e5d30adba6291821b
Taco5540 <79627951+Taco5540@users.noreply.github.com>
2022-10-17 14:44:41 +0800

    Add files via upload
    Version 1.0.1

--- autoaerovs.py
+++ autoaerovs.py
@@ -10,40 +10,57 @@ print('1 - Peformance ONLY  ')
 print('2- Complete (Characteristics & Peformance)')
 print('To select an option, either type "1" or "2" and press ENTER.')
 
-option = input() # Variable for the user's input.
+format = input() # Variable for the user's input.
 
-yes = 'Privyet, Papa.' # testing variable
-
-if option == '1':
+if format == '1':
     print("Tip: If the source doesn't specify a parameter, use 'N/A', or 'Not mentioned.'.")
     
     Plane = input('Plane name? ') # Plane name 
     BasicInfo = input('Summarized info about said aircraft? ') # Basic info about the plane
     
-    Max = input('Max speed in knots? ') # Max speed
-    Stall = input('Stall / Landing speed in knots? ') # Stall / Landing speed 
-    Range = input('Range in KM and NMI? ') # Range of the aircraft
-    Ceiling = input('Service ceiling in FT and M? ') # Service ceiling of the aircraft
+    Max = input('Max speed? ') # Max speed
+    Stall = input('Stall / Landing speed? ') # Stall / Landing speed 
+    Range = input('Range? ') # Range of the aircraft
+    Ceiling = input('Service ceiling? ') # Service ceiling of the aircraft
     
-    Wikipedia = input('Wikipedia link?') # Wikipedia link
+    Wikipedia = input('Wikipedia link? ') # Wikipedia link
 
-    # Printing | Output
-    print('**',Plane,'**', '(',BasicInfo,')')
-    print('')
-    print('**Specs**')
-    print('')
-    print('- *Performance*')
-    print('- 100%:', Max)
-    print('- Stall/Landing', Stall)
-    print('- Range', Range)
-    print('- Ceiling', Ceiling)
-    print('')
-    print('**Wikipedia Link:**', Wikipedia)
+    print("Would you like it bolded and italicized?")
+    print('1 - Yes')
+    print('2 - No')
+    
+    option1 = input()
 
-    print('Your suggestion has been loaded! Thank you for using AutoVS.') # Aesthetic 
-    print('To make another suggestion, reload this file.') # Helpful suggestion
+    if option1 == '1':
+        # Final Document | Output
+        print('**' + Plane + '**', '(' + BasicInfo +')')
+        print('')
+        print('**Specs**')
+        print('')
+        print('— *Performance:*')
+        print('- Max speed:', Max)
+        print('- Stall / Landing speed:', Stall)
+        print('- Range:', Range)
+        print('- Ceiling', Ceiling)
+        print('')
+        print('**Wikipedia Link:**', Wikipedia)
+        print('')
 
-elif option == '2':
+    elif option1 == '2':
+        print(Plane,'(' + BasicInfo +')')
+        print('')
+        print('Specs')
+        print('')
+        print('— Performance:')
+        print('- Max speed:', Max)
+        print('- Stall / Landing speed:', Stall)
+        print('- Range:', Range)
+        print('- Ceiling', Ceiling)
+        print('')
+        print('Wikipedia Link:', Wikipedia)
+        print('')
+
+elif format == '2':
 
     print("Tip: If the source doesn't specify a parameter, use 'N/A', or 'Not mentioned.'.")
     
@@ -62,30 +79,58 @@ elif option == '2':
     Range = input('Range in KM and NMI? ') # Range of the aircraft
     Ceiling = input('Service ceiling in FT and M? ') # Service ceiling of the aircraft
     
-    Wikipedia = input('Wikipedia link?') # Wikipedia link
+    Wikipedia = input('Wikipedia link? ') # Wikipedia link
 
+    print("Would you like it bolded and italicized?")
+    print('1 - Yes')
+    print('2 - No')
+    
+    option2 = input()
+
+    if option2 == "1":
     # Final document | Output
-    print('**',Plane,'**', '(',BasicInfo,')')
-    print('')
-    print('**Specs**')
-    print('')
-    print('— *Characteristics:*')
-    print('-', 'Crew:', Crew)
-    print('-', 'Capacity:', Cap)
-    print('-', 'Length:', Length)
-    print('-', 'Width:', Width)
-    print('-', 'Wingspan:', Wingspan)
-    print('-', 'Height:', Height)
-    print('')
-    print('— *Performance:*')
-    print('-', 'Max speed:', Max)
-    print('-', 'Stall / Landing speed:', Stall)
-    print('-', 'Range:', Range)
-    print('-', 'Ceiling', Ceiling)
-    print('',)
-    print('**Wikipedia Link**', Wikipedia)
+        print('**' + Plane + '**', '(' + BasicInfo +')')
+        print('')
+        print('**Specs**')
+        print('')
+        print('— *Characteristics:*')
+        print('- Crew:', Crew)
+        print('- Capacity:', Cap)
+        print('- Length:', Length)
+        print('- Width:', Width)
+        print('- Wingspan:', Wingspan)
+        print('- Height:', Height)
+        print('')
+        print('— *Performance:*')
+        print('- Max speed:', Max)
+        print('- Stall / Landing speed:', Stall)
+        print('- Range:', Range)
+        print('- Ceiling', Ceiling)
+        print('')
+        print('**Wikipedia Link**', Wikipedia)
 
-    print('Your suggestion has been loaded! Thank you for using AutoVS.') # Aesthetic 
-    print('To make another suggestion, reload this file.') # Helpful suggestion
+    elif option2 == "2":
+        print(Plane, '(' + BasicInfo +')')
+        print('')
+        print('Specs')
+        print('')
+        print('— Characteristics:')
+        print('- Crew:', Crew)
+        print('- Capacity:', Cap)
+        print('- Length:', Length)
+        print('- Width:', Width)
+        print('- Wingspan:', Wingspan)
+        print('- Height:', Height)
+        print('')
+        print('— Performance:')
+        print('- Max speed:', Max)
+        print('- Stall / Landing speed:', Stall)
+        print('- Range:', Range)
+        print('- Ceiling', Ceiling)
+        print('')
+        print('Wikipedia Link', Wikipedia)
 
-    # END
+print('Your suggestion has been loaded! Thank you for using AutoVS.') # Aesthetic 
+print('To make another suggestion, reload this file.') # Helpful suggestion
+
+# END
